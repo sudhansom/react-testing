53a75c98a13edd4e09ca65f87637ddce6bb193a2
bhuwan <bkspoudel@gmail.com>
2022-05-15 15:42:16 +0200

    tested whether the onChange function is called on the button click

---
+++ src/test/components/Toogle.test.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { render, unmountComponentAtNode } from "react-dom";
+import { act } from "react-dom/test-utils";
+
+import Toogle from "../../components/Toogle";
+
+let container = null;
+beforeEach(() => {
+  // setup a DOM element as a render target
+  container = document.createElement("div");
+  document.body.appendChild(container);
+});
+
+afterEach(() => {
+  // cleanup on existing
+  unmountComponentAtNode(container);
+  container.remove();
+  container = null;
+});
+
+it("changes vale on click", () => {
+  const onChange = jest.fn();
+  act(() => {
+    render(<Toogle onChange={onChange} />, container);
+  });
+  // get a hold of the button element and trigger click on it
+  const button = document.querySelector("[data-testid=toogle]");
+  expect(button.innerHTML).toBe("Turn on");
+  //   const button = document.getElementsByTagName("button");
+  //   expect(button[0].textContent).toBe("Turn on");
+
+  act(() => {
+    button.dispatchEvent(new MouseEvent("click", { bubbles: true }));
+  });
+  expect(onChange).toHaveBeenCalledTimes(1);
+  expect(button.innerHTML).toBe("Turn off");
+});
